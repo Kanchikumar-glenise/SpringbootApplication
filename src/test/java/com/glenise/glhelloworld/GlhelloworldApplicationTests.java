960f1455faae6e65ad6f320de5d79d97b0975efc
Kanchikumar-glenise <kanchikumar@glenise.in>
2022-06-08 20:10:48 +0530

    Application revised

--- src/test/java/com/glenise/glhelloworld/GlhelloworldApplicationTests.java
+++ src/test/java/com/glenise/glhelloworld/GlhelloworldApplicationTests.java
@@ -1,7 +1,7 @@
 package com.glenise.glhelloworld;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest.context;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GlhelloworldApplicationTests {
